86b401791cd4ac530653b2d0bbef037649125b7e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-10-08 06:10:27 +0800

    rename scribblings

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "minitt")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/minitt.scrbl" ())))
+(define scribblings '(("scribblings/racket-project.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(dannypsnl))
