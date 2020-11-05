0d67ffc20fdf3e43665312c2da8320f859f4e3db
Lîm Tsú-thuàn/林子篆/Danny <dannypsnl@gmail.com>
2020-11-06 01:09:41 +0800

    replace text in README

--- replace.sh
+++ replace.sh
@@ -2,5 +2,6 @@ new_proj_name=$1
 
 sed -i "" "s/racket-project/$new_proj_name/g" ./info.rkt
 sed -i "" "s/racket-project/$new_proj_name/g" ./scribblings/racket-project.scrbl
+sed -i "" "s/racket project/$new_proj_name/g" ./README.md
 
 mv ./scribblings/racket-project.scrbl ./scribblings/$new_proj_name.scrbl
