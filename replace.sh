4d153ed408115b91b0fe3f4455f05fc687d6ef58
Lîm Tsú-thuàn/林子篆/Danny <dannypsnl@gmail.com>
2020-11-08 05:26:39 +0800

    distinguish author and github(id) name

--- replace.sh
+++ replace.sh
@@ -1,5 +1,6 @@
 new_proj_name=$1
-new_author_name=$2 || 'dannypsnl'
+new_github_name=$2 || 'dannypsnl'
+new_authro_name=$3 || 'Lîm Tsú-thuàn'
 
 # repo name
 sed -i "" "s/racket-project/$new_proj_name/g" ./info.rkt
@@ -7,7 +8,7 @@ sed -i "" "s/racket-project/$new_proj_name/g" ./scribblings/racket-project.scrbl
 sed -i "" "s/racket project/$new_proj_name/g" ./README.md
 
 # author name
-sed -i "" "s/dannypsnl/$new_author_name/g" ./info.rkt
+sed -i "" "s/dannypsnl/$new_github_name/g" ./info.rkt
 sed -i "" "s/Lîm Tsú-thuàn/$new_author_name/g" ./scribblings/racket-project.scrbl
 
 mv ./scribblings/racket-project.scrbl ./scribblings/$new_proj_name.scrbl
