49bd63f5e7d618b49b9bb3ce5b8dd0d4a80276aa
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-11 08:06:30 +0800

    default cmdline

--- main.rkt
+++ main.rkt
@@ -1,4 +1,12 @@
 #lang racket
 
 (module+ main
-  (require racket/cmdline))
+  (require racket/cmdline)
+
+  (define who (box "world"))
+  (command-line
+    #:program "racket-project"
+    #:once-each
+    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
+    #:args ()
+    (printf "hello ~a~n" (unbox who))))
