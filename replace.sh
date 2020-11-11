9912757cfe0ee921bd023d6daaf69b5db298a573
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-11 08:02:03 +0800

    add makefile can generate gh-pages via scribble

--- replace.sh
+++ replace.sh
@@ -2,6 +2,7 @@ new_proj_name=$1
 
 # repo name
 sed -i "" "s/racket-project/$new_proj_name/g" ./info.rkt
+sed -i "" "s/racket-project/$new_proj_name/g" ./Makefile
 sed -i "" "s/racket-project/$new_proj_name/g" ./scribblings/racket-project.scrbl
 sed -i "" "s/racket project/$new_proj_name/g" ./README.md
 
