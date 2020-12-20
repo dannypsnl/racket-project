277f5bc701be1cea005842af23d6d5641393b3fc
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-20 20:49:10 +0800

    [replace] cleanup README content

--- replace.sh
+++ replace.sh
@@ -5,7 +5,7 @@ sed -i "" "s/racket-project/$new_proj_name/g" ./info.rkt
 sed -i "" "s/racket-project/$new_proj_name/g" ./main.rkt
 sed -i "" "s/racket-project/$new_proj_name/g" ./Makefile
 sed -i "" "s/racket-project/$new_proj_name/g" ./scribblings/racket-project.scrbl
-sed -i "" "s/racket project/$new_proj_name/g" ./README.md
+echo "# $new_proj_name" > README.md
 
 # author name
 if [ -z $2 ]; then echo "keep origin github id"; else sed -i "" "s/dannypsnl/$2/g" ./info.rkt; fi
