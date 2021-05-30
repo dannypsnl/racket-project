8cd3c3b738e278295edb0afa189c66b6d6aab44d
Lîm Tsú-thuàn≡悃悃 <dannypsnl@gmail.com>
2021-05-30 18:24:24 +0800

    template: for raco cmd project

--- info.rkt
+++ info.rkt
@@ -6,3 +6,6 @@
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(dannypsnl))
+
+(define raco-commands
+  '(("hello" (submod racket-project/cmd main) "hello world" #f)))
